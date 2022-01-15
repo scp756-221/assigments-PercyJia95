59b1018c184c1be09449a993d1525ee9b72c29f5
PercyJia <30244160+PercyJia95@users.noreply.github.com>
2022-01-15 09:26:25 -0800

    Add missing code to "test"

--- s2/standalone/app-a1.py
+++ s2/standalone/app-a1.py
@@ -122,7 +122,7 @@ def delete_song(music_id):
 @bp.route('/test', methods=['GET'])
 def test():
     # This value is for user scp756-221
-    if ('1e0715252b48ed14858ae1ce646d67195183ffb8f9dc02d73c82323d8d75f482' !=
+    if ('c69314133f0dfb2a79c93278335d6c10ed60498c20b03d49168db2344d579d89' !=
             ucode):
         raise Exception("Test failed")
     return {}
